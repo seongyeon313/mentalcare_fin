2598cd2fd9db094a52cab159ca821bcdbfca4ce8
AHN, Seongyeon <102126594+seongyeon313@users.noreply.github.com>
2025-05-07 16:10:03 +0900

    Update header-loader.js

--- public/js/header-loader.js
+++ public/js/header-loader.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 헤더
-    fetch('/public/partials/header.html')
+    fetch('mentalcare_fin/public/partials/header.html')
       .then(res => res.text())
       .then(html => {
         document.getElementById('header-container').innerHTML = html;
